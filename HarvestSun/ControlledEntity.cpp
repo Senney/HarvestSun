66dcd965bb98ce939ee34bc7fad65e7963994c49
Sean Heintz <bim_tim@hotmail.com>
2011-03-27 18:56:17 -0600

    Testing commit!

--- HarvestSun/ControlledEntity.cpp
+++ HarvestSun/ControlledEntity.cpp
@@ -7,3 +7,11 @@ void ControlledEntity::AddSprite(std::string animName, AnimatedSprite &sprite)
 {
 	sprites[animName] = &sprite;
 }
+
+AnimatedSprite* ControlledEntity::GetSprite(std::string index)
+{
+	if (sprites.find(index) == sprites.end)
+		return NULL;
+	else
+		return sprites[index];
+}
